c800ec6c8098ad0f33f026715f0d4376b93c0e5e
Vatsal Rajgor <rv20197@gmail.com>
2023-08-19 18:59:53 +0530

    Created Profile Image Field in Form

--- components/form/AccountProfile.tsx
+++ components/form/AccountProfile.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useForm } from 'react-hook-form';
 import { Button } from '@/components/ui/button';
-import * as z from "zod"
+import * as z from 'zod';
 import {
 	Form,
 	FormControl,
@@ -15,6 +15,8 @@ import { Input } from '@/components/ui/input';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { UserValidation } from '../../lib/validations/user';
 import { UserDefaultValues } from '../../lib/default_values/user';
+import Image from 'next/image';
+import { ChangeEvent } from 'react';
 
 type UserDataType = {
 	id: string | undefined;
@@ -40,22 +42,49 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
 		console.log(values);
 	}
 
+	function handleImage(e: ChangeEvent, fieldChange: (value: string) => void) {
+		e.preventDefault();
+	}
+
 	return (
 		<Form {...form}>
-			<form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+			<form
+				onSubmit={form.handleSubmit(onSubmit)}
+				className='flex flex-col justify-start gap-10'>
 				<FormField
 					control={form.control}
-					name='username'
+					name='profile_photo'
 					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Username</FormLabel>
-							<FormControl>
-								<Input placeholder='shadcn' {...field} />
+						<FormItem className='flex items-center gap-4'>
+							<FormLabel className='account-form_image-label'>
+								{field.value ? (
+									<Image
+										src={field.value}
+										alt='profile photo'
+										width={96}
+										height={96}
+										priority
+										className='rounded-full object-contain'
+									/>
+								) : (
+									<Image
+										src='/assets/profile.svg'
+										alt='profile photo'
+										width={24}
+										height={24}
+										className='object-contain'
+									/>
+								)}
+							</FormLabel>
+							<FormControl className='flex-1 text-base-semibol text-gray-200'>
+								<Input
+									type='file'
+									accept='image/*'
+									placeholder='Upload a photo'
+									className='account-form_image-input'
+									onChange={e => handleImage(e, field.onChange)}
+								/>
 							</FormControl>
-							<FormDescription>
-								This is your public display name.
-							</FormDescription>
-							<FormMessage />
 						</FormItem>
 					)}
 				/>
