2daeb2e3409af5d44e8748a751860da25ee75c3b
Vatsal Rajgor <rv20197@gmail.com>
2023-08-19 17:07:14 +0530

    Added assets and updated favicon

--- components/shared/TopBar.tsx
+++ components/shared/TopBar.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 type Props = {}
 
 const TopBar = (props: Props) => {
 	return (
-		<div>TopBar</div>
+		<nav className='topbar'>
+			<Link href="/" className='flex items-center gap-4'>
+				<Image src='/assets/logo.svg' alt='logo' width={28} height={28} />
+				<p className='text-heading3-bold text-light-1 max-xs:hidden'>Threads</p>
+			</Link>
+		</nav>
 	)
 }
 
