229453094fb4005358c864f57204f2304cd81e16
Vatsal Rajgor <rv20197@gmail.com>
2023-08-19 15:36:28 +0530

    Create layout for auth

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,26 @@
+import { ClerkProvider } from '@clerk/nextjs';
+import type { Metadata } from 'next';
+import { ReactNode } from 'react';
+import { Inter } from 'next/font/google';
+import '../globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+type Props = {
+	children: ReactNode;
+};
+
+export const metadata: Metadata = {
+	title: 'Threads',
+	description: 'A Next.js 13 Meta Threads Application'
+};
+
+export default function RootLayout({ children }: Props) {
+	return (
+		<ClerkProvider>
+			<html lang='en' className={`${inter.className} bg-dark-1`}>
+				<body>{children}</body>
+			</html>
+		</ClerkProvider>
+	);
+}
