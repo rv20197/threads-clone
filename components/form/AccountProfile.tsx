3841c56fb11c07e4ff8774945575560a4c40ffa4
Vatsal Rajgor <rv20197@gmail.com>
2023-08-20 09:49:16 +0530

    Fixed Styling issue

--- components/form/AccountProfile.tsx
+++ components/form/AccountProfile.tsx
@@ -94,11 +94,11 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
 					control={form.control}
 					name='name'
 					render={({ field }) => (
-						<FormItem className='flex items-center gap-3 w-full'>
+						<FormItem className='flex gap-3 w-full flex-col'>
 							<FormLabel className='text-base-semibold text-light-2'>
 								Name
 							</FormLabel>
-							<FormControl className='flex-1 text-base-semibol text-gray-200'>
+							<FormControl>
 								<Input
 									type='text'
 									className='account-form_input no-focus'
@@ -113,11 +113,11 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
 					control={form.control}
 					name='username'
 					render={({ field }) => (
-						<FormItem className='flex items-center gap-3 w-full'>
+						<FormItem className='flex gap-3 w-full flex-col'>
 							<FormLabel className='text-base-semibold text-light-2'>
 								Username
 							</FormLabel>
-							<FormControl className='flex-1 text-base-semibol text-gray-200'>
+							<FormControl>
 								<Input
 									type='text'
 									className='account-form_input no-focus'
@@ -132,11 +132,11 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
 					control={form.control}
 					name='bio'
 					render={({ field }) => (
-						<FormItem className='flex items-center gap-3 w-full'>
+						<FormItem className='flex flex-col gap-3 w-full'>
 							<FormLabel className='text-base-semibold text-light-2'>
 								Bio
 							</FormLabel>
-							<FormControl className='flex-1 text-base-semibol text-gray-200'>
+							<FormControl>
 								<Textarea
 									rows={10}
 									className='account-form_input no-focus'
