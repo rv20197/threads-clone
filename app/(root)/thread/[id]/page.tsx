ed8a1d3001611bc9202a847abbd4587097ff6182
Vatsal Dinesh Rajgor <rv20197@gmail.com>
2023-08-29 13:17:12 +0530

    Comments Section Done

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -1,9 +1,9 @@
-import ThreadCard from '@/components/cards/ThreadCard';
-import Comment from '@/components/form/Comment';
-import { fetchThreadById } from '@/lib/actions/thread.actions';
-import { fetchUser } from '@/lib/actions/user.actions';
-import { currentUser } from '@clerk/nextjs';
-import { redirect } from 'next/navigation';
+import ThreadCard from "@/components/cards/ThreadCard";
+import Comment from "@/components/form/Comment";
+import { fetchThreadById } from "@/lib/actions/thread.actions";
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 type Props = {
 	params: {
@@ -18,12 +18,12 @@ const Page = async ({ params }: Props) => {
 	if (!user) return null;
 
 	const userInfo = await fetchUser(user.id);
-	if (!userInfo?.onboardedStatus) redirect('/onboarding');
+	if (!userInfo?.onboardedStatus) redirect("/onboarding");
 
 	const thread = await fetchThreadById(params.id);
 
 	return (
-		<section className='relative'>
+		<section className="relative">
 			<div>
 				<ThreadCard
 					id={thread._id}
@@ -37,12 +37,29 @@ const Page = async ({ params }: Props) => {
 				/>
 			</div>
 
-			<div className='mt-7'>
+			<div className="mt-7">
 				<Comment
 					threadId={thread.id}
-					currentUserImg={user.imageUrl}
+					currentUserImg={userInfo.image}
 					currentUserId={JSON.stringify(userInfo._id)}
 				/>
+			</div>
+
+			<div className="mt-10">
+				{thread.children.map((child: any) => (
+					<ThreadCard
+						id={child._id}
+						currentUserId={user?.id as string}
+						parentId={child.parentId}
+						content={child.text}
+						author={child.author}
+						community={child.community}
+						createdAt={child.createdAt}
+						comments={child.children}
+						key={child._id}
+						isComment={true}
+					/>
+				))}
 			</div>
 		</section>
 	);
