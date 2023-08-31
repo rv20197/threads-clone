64ad65b8abcf6bce3e79da62bbe5ce9764fd6b73
Vatsal Dinesh Rajgor <rv20197@gmail.com>
2023-08-31 23:10:06 +0530

    Created Profile Header

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,24 +1,36 @@
-import { fetchUser } from '@/lib/actions/user.actions';
-import { currentUser } from '@clerk/nextjs';
-import { redirect } from 'next/navigation';
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-type Props={
-    params:{
-        id:string
-    }
-}
+type Props = {
+  params: {
+    id: string;
+  };
+};
 
-async function Page({params}:Props) {
-    const user = await currentUser();
+async function Page({ params }: Props) {
+  const user = await currentUser();
 
-	if (!user) return null;
+  if (!user) return null;
 
-	const userInfo = await fetchUser(params.id);
+  const userInfo = await fetchUser(params.id);
 
-	if (!userInfo?.onboardedStatus) redirect('/onboarding');
+  if (!userInfo?.onboardedStatus) redirect("/onboarding");
 
-	const userId = userInfo._id.toString();
-  return <section>Profile</section>;
+  const userId = userInfo._id.toString();
+  return (
+    <section>
+      <ProfileHeader
+        accountId={userInfo.id}
+        authUserId={user.id}
+        name={userInfo.name}
+        username={userInfo.username}
+        imgUrl={userInfo.image}
+        bio={userInfo.bio}
+      />
+    </section>
+  );
 }
 
 export default Page;
