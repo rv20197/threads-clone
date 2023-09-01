7878970b5265ead1b4d6e08f05dc1fe44155c395
Vatsal Dinesh Rajgor <rv20197@gmail.com>
2023-09-01 13:34:26 +0530

    User Model Export Corrected

--- lib/models/user.model.ts
+++ lib/models/user.model.ts
@@ -1,41 +1,42 @@
-import { Schema, model, models } from 'mongoose';
+import { Schema, model, models } from "mongoose";
 
 const userSchema = new Schema({
-	id: {
-		type: String,
-		required: true
-	},
-	username: {
-		type: String,
-		required: true,
-		unique: true
-	},
-	name: {
-		type: String,
-		required: true
-	},
-	image: {
-		type: String
-	},
-	bio: {
-		type: String
-	},
-	threads: [
-		{
-			type: Schema.Types.ObjectId,
-			ref: 'Thread'
-		}
-	],
-	onboardedStatus: {
-		type: Boolean,
-		default: false
-	},
-	communities: [
-		{
-			type: Schema.Types.ObjectId,
-			ref: 'Community'
-		}
-	]
+  id: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  bio: {
+    type: String,
+  },
+  threads: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Thread",
+    },
+  ],
+  onboardedStatus: {
+    type: Boolean,
+    default: false,
+  },
+  communities: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Community",
+    },
+  ],
 });
 
-export const User = models.User || model('User', userSchema);
+const User = models.User || model("User", userSchema);
+export default User;
