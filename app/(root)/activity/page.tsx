99bc9a21510de9b16eb198af28a09fc17b622de8
Vatsal Dinesh Rajgor <rv20197@gmail.com>
2023-09-01 12:09:43 +0530

    Added activity page

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,12 +1,49 @@
+import { fetchUser, getActivity } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 
-type Props = {}
+type Props = {};
 
 const Page = async (props: Props) => {
+  const user = await currentUser();
+
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+
+  if (!userInfo?.onboardedStatus) redirect("/onboarding");
+
+  // getActivity
+  const activity = await getActivity(userInfo._id);
   return (
     <section>
-        <h1 className="head-text mb-10">Activity</h1>
-    </section>
-  )
-}
+      <h1 className="head-text mb-10">Activity</h1>
 
-export default Page
+      <section className="mt-10 flex flex-col gap-5">
+        {activity.length > 0 ? (
+          <>
+            {activity.map((act) => (
+              <Link key={act._id} href={`/thread/${act.parentId}`}>
+                <article className="activity-card">
+                  <Image
+                    src={act.author.image}
+                    alt="profile-picture"
+                    width={20}
+                    height={20}
+                    className="rounded-full object-cover"
+                  />
+                </article>
+              </Link>
+            ))}
+          </>
+        ) : (
+          <p className="!text-base-regular text-light-3">No activity yet!</p>
+        )}
+      </section>
+    </section>
+  );
+};
+
+export default Page;
